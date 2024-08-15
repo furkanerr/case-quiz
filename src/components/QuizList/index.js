f8a013761a3a5dc9dc0129cb1eb1008afa1d9a4c
furkanerr <furkan08_@hotmail.com>
2024-08-16 00:51:32 +0300

    changes

--- src/components/QuizList/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuizListComponent = () => {
-  return (
-    <div>QuizListComponent</div>
-  )
-}
-
-export default QuizListComponent
--- src/components/quizQuesiton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const QuizQuestionComponent = () => {
-  return (
-    <div>QuizQuestion</div>
-  )
-}
-
-export default QuizQuestionComponent
